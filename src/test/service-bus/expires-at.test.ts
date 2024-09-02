a8df7afc0f9b907fe0cf194a34686390138845c0
charburgx <charburgx@gmail.com>
2024-09-01 20:20:14 -0700

    tests for expiration time

---
+++ src/test/service-bus/expires-at.test.ts
@@ -0,0 +1,32 @@
+import { fixturedTest } from "test/fixtured-test.js"
+
+fixturedTest(
+  "message has accurate expires at time",
+  async ({ onTestFinished, azure_queue, expect }) => {
+    const { sb_client, createQueue } = azure_queue
+
+    const queue = await createQueue("queue", {})
+
+    const sender = sb_client.createSender(queue.name!)
+    onTestFinished(() => sender.close())
+
+    const receiver = sb_client.createReceiver(queue.name!)
+    onTestFinished(() => receiver.close())
+
+    const ttlMs = 100000
+
+    await sender.sendMessages({
+      body: "hello world!",
+      timeToLive: ttlMs,
+    })
+
+    {
+      const [message] = await receiver.peekMessages(1)
+      expect(message).toBeTruthy()
+
+      expect(message?.expiresAtUtc).toStrictEqual(
+        new Date(message!.enqueuedTimeUtc!.getTime() + ttlMs),
+      )
+    }
+  },
+)
